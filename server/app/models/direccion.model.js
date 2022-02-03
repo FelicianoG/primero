ec32e59fb96cfe6dcc614f5b7f7cbe1c2b4a895b
Luis Garcia <lugar.blanco@gmail.com>
2022-02-02 19:55:39 -0700

    test crate endpoint with postman

---
+++ server/app/models/direccion.model.js
@@ -0,0 +1,28 @@
+module.exports = mongoose =>{
+    var dirSchema = mongoose.Schema(
+            {
+                calle: String,
+                numero: String,
+                colonia: String,
+                cp: String,
+                ciudad: String,
+                estado: String,
+                pais: String 
+            },
+            {timestamps : true}
+        
+    );
+
+
+    dirSchema.method("toJSON", function() {
+        const { _id, ...object} = this.toObject();
+        object.id = _id;
+        return object;
+    });
+
+    const Direccion = mongoose.model("direcciones", dirSchema);
+
+    return Direccion;
+};
+
+
---
+++ server/app/routes/empleado.routes.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const router = express.Router();
+
+
+const {create, findAll, update, findOne} = require("../controllers/empleados.controller.js"); 
+
+router.get('/', findAll);
+
+router.get("/:id", findOne);
+
+router.post('/', create);
+
+router.put('/:id', update);
+
+// router.delete('/:id', delete);
+
+// router.delete("/", deleteAll);
+
+module.exports = router;
