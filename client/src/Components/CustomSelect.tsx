ad296a28a9f0a90dacea4d4653bcf846f6c2cbff
Luis Garcia <lugar.blanco@gmail.com>
2022-02-11 09:15:54 -0700

    - #add default values to custom fields

--- client/src/Components/CustomSelect.tsx
+++ client/src/Components/CustomSelect.tsx
@@ -1,5 +1,5 @@
 import Select from "@mui/material/Select";
-import { Controller, useFormContext} from "react-hook-form";
+import { Controller, useFormContext } from "react-hook-form";
 import MenuItem from "@mui/material/MenuItem";
 import InputLabel from "@mui/material/InputLabel";
 import FormControl from "@mui/material/FormControl";
@@ -8,45 +8,43 @@ import { FormHelperText } from "@mui/material";
 type opciones = {
   valor: number;
   label: string;
-}
+};
 
 interface CustomSelectProps {
-    opciones:opciones[];
-    name:string;
-    label:string;
-    required?:boolean;
+  defaultValue?: string | undefined | null;
+  opciones: opciones[];
+  name: string;
+  label: string;
+  required?: boolean;
 }
 
-export default function CustomSelect({
-    opciones,
-    name, 
-    label,
-    required,
-}: CustomSelectProps) {
+export default function CustomSelect({ defaultValue, opciones, name, label, required }: CustomSelectProps) {
+  const formContext = useFormContext();
+  const { control } = formContext;
 
-    const formContext = useFormContext();
-    const { control } = formContext;
-  
-    return (
-        <div>
-          <Controller
-            name={name}
-            control={control}
-            rules={{required : required ? 'este campo es obligatorio' : undefined}}
-            render={({ field, fieldState: {error} }) => 
-                <FormControl fullWidth >
-                    <InputLabel id="labelId" error={!!error}>{label}</InputLabel>
-                    <Select error={!!error} id="labelId" label={label} variant="outlined" {...field} >
-                        {opciones.map((opcion, key) => 
-                            <MenuItem key={key} value={opcion.valor}>{opcion.label}</MenuItem> )
-                        }
-                    </Select>
-                    <FormHelperText error={!!error}>
-                        {error ? error.message : null}
-                    </FormHelperText>
-                </FormControl>}
-            />
-          
-        </div>
-      );
-    }
+  return (
+    <div>
+      <Controller
+        name={name}
+        control={control}
+        defaultValue={defaultValue ?? ""}
+        rules={{ required: required ? "este campo es obligatorio" : undefined }}
+        render={({ field, fieldState: { error } }) => (
+          <FormControl fullWidth>
+            <InputLabel id="labelId" error={!!error}>
+              {label}
+            </InputLabel>
+            <Select error={!!error} id="labelId" label={label} variant="outlined" {...field}>
+              {opciones.map((opcion, key) => (
+                <MenuItem key={key} value={opcion.valor}>
+                  {opcion.label}
+                </MenuItem>
+              ))}
+            </Select>
+            <FormHelperText error={!!error}>{error ? error.message : null}</FormHelperText>
+          </FormControl>
+        )}
+      />
+    </div>
+  );
+}
--- client/src/Components/CustomTextField.tsx
+++ client/src/Components/CustomTextField.tsx
@@ -2,30 +2,26 @@ import TextField from "@mui/material/TextField";
 import { Controller, useFormContext } from "react-hook-form";
 
 interface CustomTextfieldProps {
-  
+  defaultValue?: string | number | readonly string[] | undefined | null;
   label: string;
-  name:string;
+  name: string;
   required?: boolean;
 }
 
-export default function CustomTextfield({
-  
-  label,
-  name,
-  required,
-
-}: CustomTextfieldProps) {
- 
+export default function CustomTextfield({ defaultValue, label, name, required }: CustomTextfieldProps) {
   const formContext = useFormContext();
   const { control } = formContext;
 
   return (
     <div>
       <Controller
+        defaultValue={defaultValue ?? ""}
         name={name}
-        rules={{required: required ? 'este campo es obligatorio' : undefined }}
+        rules={{ required: required ? "este campo es obligatorio" : undefined }}
         control={control}
-        render={({ field, fieldState: {error} }) => <TextField error={!!error} helperText={error ? error.message : null} label={label} {...field } />}
+        render={({ field, fieldState: { error } }) => (
+          <TextField error={!!error} helperText={error ? error.message : null} label={label} {...field} />
+        )}
       />
     </div>
   );
