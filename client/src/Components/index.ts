cb061b463ccbcf0986906eca596ff1e33fe82d2a
Luis Garcia <lugar.blanco@gmail.com>
2022-02-18 19:57:59 -0700

    add index.ts

---
+++ client/src/Components/index.ts
@@ -0,0 +1,9 @@
+export {default as CustomAutocomplete } from './CustomAutocomplete';
+export {default as CustomCheckbox } from './CustomCheckbox';
+export {default as CustomRandio } from './CustomRadio';
+export {default as CustomRating } from './CustomRating';
+export {default as CustomSelect } from './CustomSelect';
+export {default as CustomTextArea } from './CustomTextArea';
+export {default as CustomTextField } from './CustomTextField';
+export {default as CustomHiddenField } from './CustomHiddenField';
+export {default as  Wait } from './Wait';
